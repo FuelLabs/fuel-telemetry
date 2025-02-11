66d16ea6e3d484e2dd293352e1bda5e98f6c46e8
Alfie John <alfie@alfie.wtf>
2025-02-11 23:50:13 +1100

    Added comments to better explain how forc-telemetry works

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,12 +1,32 @@
 use forc_telemetry::prelude::*;
 
 fn main() {
+    // This one-liner hides most of the tracing and telemetry boilerplate code:
+    // - Sets `TelemetryLayer` as the default `tracing` `Subscriber`
+    // - Creates the root `Span` with the following settings:
+    //     - name: `main`
+    //     - level: `Level::ERROR`
+    //     - telemetry: true
+    // - Starts a `FileWatcher` to poll the filesystem for aged-out telemetry
+    //   files and then submit them to InfluxDB
+
     telemetry_init().unwrap();
 
+    // Create a tracing `Span` that will also be submitted to InfluxDB
     info!("An event with span 'main' is recorded since telemetry_init() sets telemetry=true");
 
     test_a();
 }
+
+// When a function has an attribute in the form
+// `#[tracing::instrument(fields(telemetry = true))]`, function calls create a
+// an `Event` on entry along with the following settings:
+// - span name: `<function_name>`
+// - telemetry: true
+// - fields: function argument name and values used during the call
+//
+// To hide sensitive function parameters from being recorded, the skip attribute parameter can be used:
+// `#[tracing::instrument(fields(telemetry = true), skip(<sensitive_parameter_name>))]`
 
 #[tracing::instrument(fields(telemetry = false))]
 fn test_a() {
@@ -19,6 +39,9 @@ fn test_a() {
 }
 
 pub fn test_b() {
+    // As there was no function argument for `test_b()`, we fall back to the
+    // current span (which would be `main:test_a`), however as the `test_a()`
+    // function attribute sets telemetry=false, this event will be ignored`
     info!("An event with span 'main:test_a' is ignored since test_a()'s attribute sets telemetry=false");
 }
 
@@ -35,6 +58,7 @@ pub fn test_d() {
 pub fn test_e() {
     info!("An event with span 'main:test_a:test_e' is ignored since test_e()'s attribute sets telemetry=false");
 
+    // We can also create `Span`s manually rather than from a function attribute:
     let level_e_span = span!(Level::ERROR, "level_e", telemetry = true);
     let _level_e_guard = level_e_span.enter();
 
