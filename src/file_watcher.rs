8140010688646ba70a096460fd540e98f920ec7a
Alfie John <alfie@alfie.wtf>
2025-02-17 16:00:12 +1100

    Fixes for clippy

--- src/file_watcher.rs
+++ src/file_watcher.rs
@@ -324,7 +324,9 @@ fn find_telemetry_files(ignore_age: bool) -> Result<Vec<PathBuf>> {
         .filter(|p| p.is_file())
         .filter(|p| p.to_string_lossy().contains(".telemetry."))
         .filter_map(|path| {
-            if ignore_age || poll_interval < path.metadata().ok()?.modified().ok()?.elapsed().ok()? {
+            if ignore_age
+                || poll_interval < path.metadata().ok()?.modified().ok()?.elapsed().ok()?
+            {
                 Some(Ok(path))
             } else {
                 None
