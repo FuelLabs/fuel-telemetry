5ef4380a62665fbcdd6d8ceb89d235ee1bca1d50
Alfie John <alfie@alfie.wtf>
2025-03-05 07:02:04 +1100

    Add comments to proc_macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,6 +2,12 @@ use quote::quote;
 
 extern crate proc_macro;
 
+// Sets environment variables from the cargo target's perspective
+//
+// These values need to come from the cargo target which can only be found
+// during macro expansion. Calling `env!('CARGO_PKG_NAME')` within
+// `telemetry_layer.rs` will be incorrect as the macro will have already
+// expanded leading to the constant value "fuel-telemetry" for all targets
 fn set_env_vars() -> proc_macro2::TokenStream {
     quote! {
         if std::env::var("TELEMETRY_PKG_NAME").is_err() {
@@ -14,6 +20,7 @@ fn set_env_vars() -> proc_macro2::TokenStream {
     }
 }
 
+// Starts the `FileWatcher` and `SystemInfoWatcher` daemon
 fn start_watchers() -> proc_macro2::TokenStream {
     quote! {
         let _ = fuel_telemetry::file_watcher::FileWatcher::new()
@@ -62,6 +69,9 @@ pub fn new_with_watchers_and_init(_input: proc_macro::TokenStream) -> proc_macro
     let mut crate_name = String::new();
     let mut crate_type = String::new();
 
+    // During macro expansion, we extract the crate name and type from the
+    // compiler's command line arguments, as this is the only time and place
+    // this information is available.
     for window in args.windows(2) {
         match window[0].as_str() {
             "--crate-name" => crate_name = window[1].clone(),
@@ -70,6 +80,8 @@ pub fn new_with_watchers_and_init(_input: proc_macro::TokenStream) -> proc_macro
         }
     }
 
+    // If the crate is a library, and it is not the `fuel_telemetry` crate,
+    // then generate a compiler error so we cannot continue!
     if crate_type == "lib" && crate_name != "fuel_telemetry" {
         return quote! {
             {
